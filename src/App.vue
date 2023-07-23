c8d62c42eb20848e63d66551e97980c44a8cbd25
Arman <md.armantext@gmail.com>
2023-07-23 22:55:41 +0600

    complite the module_04 assinment

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+  const count = ref(0);
+  const fullImage = ref("https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60");
+  const images = reactive([
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+
+  
+])
+
+function leftIcon(){
+  count.value = count.value - 1
+  const decerementValue = count.value
+
+  if(decerementValue < 0 ){
+      fullImage.value = images[0].full
+      console.log(count.value = 0)
+  }
+  fullImage.value = images[decerementValue].full
+
+}
+
+ function rightIcon(){
+    count.value = count.value + 1
+   const incrimentValue = count.value
+
+    if(incrimentValue < images.length ){
+      fullImage.value = images[incrimentValue].full
+    }
+    if(incrimentValue >= images.length){
+      fullImage.value = images[0].full
+      count.value = 0
+    }
+
+}
+
+
+</script>
+
+<template>
+  <section class="container mx-auto flex items-center flex-col pt-0">
+    <h1 class="text-center text-2xl pb-2">Events & Reactivity</h1>
+    <div class="w-full flex space-x-3" >
+        <img :src="image.thumb" class="w-1/4" v-for="image in images" :key="image.thumb">
+    </div>
+    <div class="w-full pt-10">
+      <div class="flex justify-around">
+        <div @click="leftIcon()" class="my-auto cursor-pointer">
+            <svg width="40px" height="40px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="#000000" /></svg>
+        </div>
+
+          <div class="w-full m-auto">
+            <img :src="fullImage" class="w-1/2 m-auto">
+          </div>
+
+          <div @click="rightIcon()" class="my-auto cursor-pointer">
+              <svg width="40px" height="40px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" fill="#000000" /></svg>
+          </div>
+         
+      </div>
+    </div>
+    
+  </section>
+</template>
+
+<style scoped></style>
